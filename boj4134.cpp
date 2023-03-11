fb4d46ca3bc0071a5643a08eedb8f42cf80e6ff4
Jihun Choi <chl020729@gmail.com>
2023-03-11 22:53:04 +0900

    2023/03/11

--- boj4134.cpp
+++ boj4134.cpp
@@ -1,30 +1,31 @@
 #include <bits/stdc++.h>
-#define fast ios::sync_with_stdio(0),cin.tie(nullptr),cout.tie(nullptr)
+#define fast ios::sync_with_stdio(0), cin.tie(nullptr), cout.tie(nullptr)
 #define endl '\n'
 /**
     https://www.acmicpc.net/problem/4134
     4134번 다음 소수
 */
 using namespace std;
-bool prime(int n){
-    if(n==1)return false;
-    for(int i=2;i*i<=n;i++){
-        if(n%i==0)return false;
-    }    
+bool prime(long long n) {
+    if (n == 1 || n == 0) return false;
+    for (long long i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
     return true;
 }
-int n;
-int main(){
+long long n;
+int main() {
     fast;
     cin >> n;
-    while(n--){
-        int number;
+    while (n--) {
+        long long number;
         cin >> number;
-        for(int i=number;;i++){
-            if(prime(i)){
-            cout << i << endl;
-            break;
+        while(1){
+            if(prime(number)){
+                cout << number << endl;
+                break;
             }
+            number++;
         }
     }
     return 0;
