6940fb6ab2cac81412c211ce00172273dffd8ef1
choijihun <chl020729@gmail.com>
2023-06-29 20:20:28 +0900

    2023/06/29

---
+++ boj24060.c
@@ -0,0 +1,92 @@
+#include <stdlib.h>
+/*
+ https://www.acmicpc.net/problem/24060
+ 24060번 알고리즘 수업 - 병합 정렬 1
+*/
+typedef long long ll;
+
+typedef struct node{//해당하는 값을 arr로 갖는다.
+    int v;
+}node;
+
+typedef struct vector{//인덱스 접근 가능해야 하며, size 존재, back, pop_back,
+    node* arr;//값을 저장하는 곳
+    ll size;//크기
+    ll capacity;//전체 크기
+}vector;
+
+void init(vector *q){
+    q->arr=(node *)malloc(sizeof(node)*(64));
+    q->size=0;
+    q->capacity=64;//초기 크기 64개
+}
+
+void push_back(vector *q,int v){//v가 추가되기.
+    if(q->size+1<=q->capacity){//
+        q->arr[q->size].v=v;
+    }else{
+        ll new_size= q->capacity*2;
+        q->arr = (node*)realloc(q->arr,sizeof(node)*new_size);
+        q->arr[q->size].v=v;
+        q->capacity=new_size;
+    }
+    q->size++;//크기 증가
+}
+
+void pop_back(vector *q){
+    if(q->size){//없을 때는 불러내면 안됨.
+        q->size--;//값 줄이기
+    }else{
+        printf("size가 0이므로 더이상 못 뺌\n");
+    }
+}
+
+node* back(vector *q){
+    node* ret = &q->arr[q->size-1];
+    return ret;//값 넘겨주기
+}
+int cmp(void*a,void*b){
+    return ((node *)a)->v-((node *)b)->v;
+}
+int n,k;
+ll cnt;
+void merge(vector*arr,int front,int mid,int back){
+    int i=front,j=mid+1,t=1;
+    vector *tmp=(vector*)malloc(sizeof(vector));init(tmp);
+    while(i<=mid && j<=back){
+        if(arr->arr[i].v<=arr->arr[j].v){
+            push_back(tmp, arr->arr[i++].v);
+        }else{
+            push_back(tmp, arr->arr[j++].v);
+        }
+    }
+    while(i<=mid){
+        push_back(tmp, arr->arr[i++].v);
+    }
+    while(j<=back){
+        push_back(tmp, arr->arr[j++].v);
+    }
+    i=front,t=0;
+    while(i<=back){
+        arr->arr[i++].v = tmp->arr[t++].v;
+        cnt++;
+        if(cnt==k){
+            printf("%d",arr->arr[i-1].v);
+            exit(0);
+        }
+    }
+}
+void merge_sort(vector*arr,int front,int back){
+    if(front < back){
+        int mid = (front+back)/2;
+        merge_sort(arr,front,mid);
+        merge_sort(arr,mid+1,back);
+        merge(arr,front,mid,back);
+    }
+}
+main(){scanf("%d %d",&n,&k);
+    vector* arr = (vector*)malloc(sizeof(vector));init(arr);
+    for(int i=0,t;i<n;i++)scanf("%d",&t),push_back(arr,t);
+    merge_sort(arr,0,n-1);
+    printf("-1");
+}
