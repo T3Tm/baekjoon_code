f329e3df1c223469a692b609f107855aaafca8e3
choijihun <chl020729@gmail.com>
2023-09-07 14:11:57 +0900

    2023/09/07

---
+++ boj14468.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define fast ios::sync_with_stdio(0),cin.tie(0)
+#define endl '\n'
+
+/*
+	https://www.acmicpc.net/problem/14468
+	14468번 소가 길을 건너간 이유 2
+*/
+using namespace std;
+int main() {
+	fast; set<pair<int, int>>g;
+	string a; cin >> a;
+	int idx[27]{};
+	int cnt[27]{};
+	for (int i = 0; i < a.size(); i++) {
+		cnt[a[i] - 'A']++;
+		if (cnt[a[i] - 'A'] == 2) {
+			for (int j = idx[a[i] - 'A'] + 1; j < i; j++) {
+				if (cnt[a[j] - 'A'] == 1)g.insert({ a[i],a[j] });
+			}
+		}
+		idx[a[i] - 'A'] = i;//idx
+	}
+	cout << g.size();
+}
